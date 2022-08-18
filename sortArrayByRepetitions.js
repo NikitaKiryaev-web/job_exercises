612c2729d9d1aa53ec0225e30ca2a3bfecbf2d0d
Nikita <kiryaev.work@yandex.ru>
2022-08-18 14:38:58 +0300

    task

---
+++ sortArrayByRepetitions.js
@@ -0,0 +1,11 @@
+// Получить новый массив отсортированных по количеству повторений значений
+
+const arr = [1, 1, 2, 2, 2, 5, 5, 5, 2, 7];
+
+const elemMap = new Map();
+arr.forEach((elem) => {
+  elemMap.set(elem, elemMap.has(elem) ? elemMap.get(elem) + 1 : 1);
+});
+
+let newArr = [...elemMap].sort((a, b) => b[1] - a[1]);
+console.log(newArr.map((elem) => elem[0]));
